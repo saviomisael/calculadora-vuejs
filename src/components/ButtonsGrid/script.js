1fe5b8b8f66ae1378dbc63aa0f3af2907cb886b5
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-21 20:19:22 -0300

    add backspace icon

--- src/components/ButtonsGrid/script.js
+++ src/components/ButtonsGrid/script.js
@@ -1,7 +1,13 @@
 import OperatorButton from '../OperatorButton'
 import DigitButton from '../DigitButton'
+import backspaceIcon from '../../assets/images/backspace.svg'
 
 export default {
+  data() {
+    return {
+      backspaceIcon,
+    }
+  },
   components: {
     OperatorButton,
     DigitButton,
