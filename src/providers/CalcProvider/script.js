0300ef0f18fdf899e0fee79aeb4bc59136032c20
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-22 12:40:13 -0300

    add appendFormula / add addOperator

--- src/providers/CalcProvider/script.js
+++ src/providers/CalcProvider/script.js
@@ -1,6 +1,39 @@
+import { provide, ref, readonly } from 'vue'
+import { getFinalCharacter } from '../../utils/getFinalCharacter'
+import { replaceFinalCharacter } from '../../utils/replaceFinalCharacter'
+
 export default {
-  provide: {
-    formula: '',
-    result: '',
+  name: 'CalcProvider',
+  setup() {
+    const formula = ref('')
+    const result = ref('')
+
+    provide('formula', readonly(formula))
+    provide('result', readonly(result))
+
+    provide('appendFormula', (value) => {
+      formula.value = `${formula.value}${value}`
+    })
+
+    provide('addOperator', (operator) => {
+      if (!formula.value && operator !== '√') return
+
+      if (!formula.value && operator === '√') {
+        formula.value = `${operator} `
+        return
+      }
+
+      if (formula.value && getFinalCharacter(formula.value) === '√') return
+
+      if (
+        formula.value &&
+        ['÷', '×', '-', '+'].includes(getFinalCharacter(formula.value))
+      ) {
+        formula.value = replaceFinalCharacter(formula.value, operator)
+        return
+      }
+
+      formula.value = `${formula.value} ${operator} `
+    })
   },
 }
