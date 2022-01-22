aa6d46fe7b18f58800955fdee191ce22bbd4fecd
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-22 18:13:49 -0300

    refactor PanelResult component

--- src/components/PanelResult/script.js
+++ src/components/PanelResult/script.js
@@ -1,3 +1,5 @@
+import { formatFormulaNumbers, formatNumber } from '../../utils/formatNumber'
+
 export default {
   name: 'PanelResult',
   props: {
@@ -16,6 +18,12 @@ export default {
   computed: {
     formulaVariants() {
       return { 'panel-result__formula--small': Boolean(this.result) }
+    },
+    resultFormatted() {
+      return this.result && formatNumber(this.result)
+    },
+    formulaFormatted() {
+      return this.formula && formatFormulaNumbers(this.formula)
     },
   },
 }
