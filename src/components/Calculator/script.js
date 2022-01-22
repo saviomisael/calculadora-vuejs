83c1433ca5b7030e8c11020bb818ff38635242d7
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-22 12:40:37 -0300

    refactor Calculator

--- src/components/Calculator/script.js
+++ src/components/Calculator/script.js
@@ -1,10 +1,20 @@
+import { inject } from 'vue'
 import PanelResult from '../PanelResult'
 import ButtonsGrid from '../ButtonsGrid'
 
 export default {
+  name: 'Calculator',
+  setup() {
+    const formula = inject('formula')
+    const result = inject('result')
+
+    return {
+      formula,
+      result,
+    }
+  },
   components: {
     PanelResult,
     ButtonsGrid,
   },
-  inject: ['formula', 'result'],
 }
