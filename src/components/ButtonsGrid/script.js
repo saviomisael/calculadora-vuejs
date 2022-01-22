d3c966f3b4ecaf072cf13a556183344b8a56173b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-22 12:41:12 -0300

    refactor ButtonsGrid

--- src/components/ButtonsGrid/script.js
+++ src/components/ButtonsGrid/script.js
@@ -1,8 +1,19 @@
+import { inject } from 'vue'
 import OperatorButton from '../OperatorButton'
 import DigitButton from '../DigitButton'
 import backspaceIcon from '../../assets/images/backspace.svg'
 
 export default {
+  name: 'ButtonsGrid',
+  setup() {
+    const appendFormula = inject('appendFormula')
+    const addOperator = inject('addOperator')
+
+    return {
+      appendFormula,
+      addOperator,
+    }
+  },
   data() {
     return {
       backspaceIcon,
@@ -11,5 +22,13 @@ export default {
   components: {
     OperatorButton,
     DigitButton,
+  },
+  methods: {
+    handleDigitClick(digit) {
+      this.appendFormula(digit)
+    },
+    handleOperatorClick(operator) {
+      this.addOperator(operator)
+    },
   },
 }
