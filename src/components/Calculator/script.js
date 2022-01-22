52029e29b884969625ba386e523841f1a57cbaf3
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-22 17:33:17 -0300

    refactor Calculator component

--- src/components/Calculator/script.js
+++ src/components/Calculator/script.js
@@ -1,20 +1,31 @@
 import { inject } from 'vue'
 import PanelResult from '../PanelResult'
 import ButtonsGrid from '../ButtonsGrid'
+import ModalError from '../ModalError'
 
 export default {
   name: 'Calculator',
   setup() {
     const formula = inject('formula')
     const result = inject('result')
+    const occurDivisionPerZero = inject('occurDivisionPerZero')
+    const clearDivPerZero = inject('clearDivPerZero')
 
     return {
       formula,
       result,
+      occurDivisionPerZero,
+      clearDivPerZero,
     }
   },
   components: {
     PanelResult,
     ButtonsGrid,
+    ModalError,
+  },
+  methods: {
+    handleCloseModalClick() {
+      this.clearDivPerZero()
+    },
   },
 }
