4a87a894462c248bc6d30824b66528691b5d2598
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-22 12:39:22 -0300

    refactor OperatorButton

--- src/components/OperatorButton/script.js
+++ src/components/OperatorButton/script.js
@@ -5,17 +5,10 @@ export default {
       type: String,
       required: true,
     },
-    operation: {
-      type: String,
-      required: true,
-      validator(value) {
-        return ['square', 'div', 'mult', 'sub', 'add'].includes(value)
-      },
-    },
   },
   methods: {
     handleOperatorClick(event) {
-      this.$emit('on-operator-click', event.target.dataset.operation)
+      this.$emit('on-operator-click', this.operatorSymbol)
     },
   },
   emits: ['on-operator-click'],
