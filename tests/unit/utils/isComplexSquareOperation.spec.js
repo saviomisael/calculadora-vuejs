a6560d125ded3155f309990d73750da139fec2ee
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-26 13:05:12 -0300

    pass test should return false when formula does not end with a complex square operation

--- tests/unit/utils/isComplexSquareOperation.spec.js
+++ tests/unit/utils/isComplexSquareOperation.spec.js
@@ -18,4 +18,22 @@ describe('isComplexSquareOperation', () => {
 
     expect(result).toBeTruthy()
   })
+
+  it('should return false when formula does not end with a complex square operation', () => {
+    let result = isComplexSquareOperation('1 + 2')
+
+    expect(result).toBeFalsy()
+
+    result = isComplexSquareOperation('√ 9 + 1')
+
+    expect(result).toBeFalsy()
+
+    result = isComplexSquareOperation('√ 9 + ')
+
+    expect(result).toBeFalsy()
+
+    result = isComplexSquareOperation('√')
+
+    expect(result).toBeFalsy()
+  })
 })
