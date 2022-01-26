348f20259ed9e75822d90fe366d865a92536ba20
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-26 13:10:55 -0300

    pass test should replaces every division operator to math format

---
+++ tests/unit/utils/toMathFormulaFormat.spec.js
@@ -0,0 +1,9 @@
+import { toMathFormulaFormat } from '../../../src/utils/toMathFormulaFormat'
+
+describe('toMathFormulaFormat', () => {
+  it('should replaces every division operator to math format', () => {
+    const result = toMathFormulaFormat('9 ÷ 3 ÷ 1')
+
+    expect(result).toBe('9 / 3 / 1')
+  })
+})
