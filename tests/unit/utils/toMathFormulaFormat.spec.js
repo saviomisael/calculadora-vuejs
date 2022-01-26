be85e38eac7426b01aa365f954cecbec812e106f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-26 13:16:42 -0300

    pass test should replaces every square root operator to math format

--- tests/unit/utils/toMathFormulaFormat.spec.js
+++ tests/unit/utils/toMathFormulaFormat.spec.js
@@ -12,4 +12,10 @@ describe('toMathFormulaFormat', () => {
 
     expect(result).toBe('9 * 3 * 1')
   })
+
+  it('should replaces every square root operator to math format', () => {
+    const result = toMathFormulaFormat('√ 9 √ -3 √ -1.02 √ 3.')
+
+    expect(result).toBe('sqrt(9) sqrt(-3) sqrt(-1.02) sqrt(3.)')
+  })
 })
