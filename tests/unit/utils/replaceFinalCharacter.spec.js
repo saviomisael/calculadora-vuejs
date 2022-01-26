8a4b1695bf31b56b57e85a4667ee8e475d4535b6
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-26 11:38:27 -0300

    pass test should replace final character when formula ends with minus operator

--- tests/unit/utils/replaceFinalCharacter.spec.js
+++ tests/unit/utils/replaceFinalCharacter.spec.js
@@ -18,4 +18,22 @@ describe('replaceFinalCharacter', () => {
 
     expect(result).toBe('1 × ')
   })
+
+  it('should replace final character when formula ends with minus operator', () => {
+    let result = replaceFinalCharacter('1 - ', '-')
+
+    expect(result).toBe('1 - ')
+
+    result = replaceFinalCharacter('1 - ', '÷')
+
+    expect(result).toBe('1 ÷ ')
+
+    result = replaceFinalCharacter('1 - ', '+')
+
+    expect(result).toBe('1 + ')
+
+    result = replaceFinalCharacter('1 - ', '×')
+
+    expect(result).toBe('1 × ')
+  })
 })
