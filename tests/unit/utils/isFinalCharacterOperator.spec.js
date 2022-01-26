d771a984362d9179b1fc21959073ecf665b3a29e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2022-01-26 11:17:06 -0300

    pass test should return true when final character is a operator valid

--- tests/unit/utils/isFinalCharacterOperator.spec.js
+++ tests/unit/utils/isFinalCharacterOperator.spec.js
@@ -10,4 +10,30 @@ describe('isFinalCharacterOperator', () => {
 
     expect(result).toBeFalsy()
   })
+
+  it('should return true when final character is a operator valid', () => {
+    let result = isFinalCharacterOperator('√')
+
+    expect(result).toBeTruthy()
+
+    result = isFinalCharacterOperator('1 ÷ ')
+
+    expect(result).toBeTruthy()
+
+    result = isFinalCharacterOperator('1 × ')
+
+    expect(result).toBeTruthy()
+
+    result = isFinalCharacterOperator('1 - ')
+
+    expect(result).toBeTruthy()
+
+    result = isFinalCharacterOperator('1 + ')
+
+    expect(result).toBeTruthy()
+
+    result = isFinalCharacterOperator('1 - 2=')
+
+    expect(result).toBeTruthy()
+  })
 })
